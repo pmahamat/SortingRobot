7f58dea3e74946d91f983ef0d42aad9c6110986e
Thomas Oosterhoff <thomas.oosterhoff@gmail.com>
2018-04-24 14:01:57 +0200

    logger

--- src/Test/Logger.java
+++ src/Test/Logger.java
@@ -1,4 +1,18 @@
 package Test;
 
+import java.awt.*;
+
 public class Logger {
+
+    public static void Log(Color kleur1, Color kleur2, Color kleur3, Color kleur4){
+
+    }
+
+    public static void Log(int kleur1, int kleur2, int kleur3, int kleur4){
+
+    }
+
+    public static void Log(String action){
+
+    }
 }
