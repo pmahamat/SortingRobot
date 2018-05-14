31623bb16af8ce52c145798c0025a51dbe9e3b49
Svenyie <33426376+Svenyie@users.noreply.github.com>
2018-05-14 13:25:36 +0200

    Logger bijgewerkt

--- src/Test/Logger.java
+++ src/Test/Logger.java
@@ -3,16 +3,18 @@ package Test;
 import java.awt.*;
 
 public class Logger {
+    Database Database = new Database();
 
-    public static void Log(Color kleur1, Color kleur2, Color kleur3, Color kleur4){
-
+    public void LogSamenstelling(int bak1, int bak2, int bak3, int bak4,String naam){
+        Database.insertSamenstelling(naam,bak1,bak2,bak3,bak4);
     }
 
-    public static void Log(int kleur1, int kleur2, int kleur3, int kleur4){
 
+    public void LogRobot(int bak1, int bak2, int bak3, int bak4){
+        Database.insertRobot(bak1,bak2,bak3,bak4);
     }
 
-    public static void Log(String action){
-
+    public void Log(String bericht,int componentID){
+        Database.insertLog(bericht,componentID);
     }
 }
